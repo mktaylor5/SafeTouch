76c8756eb1557f87be95ea76f0693083d04ec7f5
mktaylor5 <mk.taylor654@gmail.com>
2018-03-01 18:13:12 -0600

    Menu changes - from three dot drop down to images across the action bar
    Added necessary image files for material icons on menu

--- app/src/main/java/com/safetouch/MainActivity.java
+++ app/src/main/java/com/safetouch/MainActivity.java
@@ -2,6 +2,9 @@ package com.safetouch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.emergency_contacts) {
+            Toast.makeText(this, "Contacts", Toast.LENGTH_SHORT).show();
+        }
+        else if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+        //return super.onOptionsItemSelected(item);
     }
 }
