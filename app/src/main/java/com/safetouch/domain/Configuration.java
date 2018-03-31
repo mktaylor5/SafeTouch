82b896b883ca4f9a3900f3fe6d899da45981f7a7
mktaylor5 <mk.taylor654@gmail.com>
2018-03-31 17:12:38 -0500

    Configuration database model complete

--- app/src/main/java/com/safetouch/domain/Configuration.java
+++ app/src/main/java/com/safetouch/domain/Configuration.java
@@ -4,6 +4,8 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 
+import java.sql.Time;
+
 /**
  * Created by mktay on 3/5/2018.
  */
@@ -28,7 +30,64 @@ public class Configuration {
     public String getEmergencyText() { return EmergencyText; }
     public void setEmergencyText(String emergencyText) { this.EmergencyText = emergencyText; }
 
+    // Sound Alarm Column Definition
+    @ColumnInfo(name = "SoundAlarm")
+    private boolean SoundAlarm;
+
+    // Sound Alarm Methods
+    public boolean getSoundAlarm() { return SoundAlarm; }
+    public void setSoundAlarm(boolean soundAlarm) { this.SoundAlarm = soundAlarm; }
+
+    // Current Mode Column Definition
+    @ColumnInfo(name = "AppMode")
+    private Mode AppMode;
+
+    // Current Mode Methods
+    public Mode getAppMode() { return AppMode; }
+    public void setAppMode(Mode mode) { this.AppMode = mode; }
+
+    // Check In Interval Column Definition
+    @ColumnInfo(name = "CheckInInterval")
+    private int CheckInInterval;
+
+    // Check In Interval Methods
+    public int getCheckInInterval() { return CheckInInterval; }
+    public void setCheckInInterval(int checkInInterval) { this.CheckInInterval = checkInInterval; }
+
+    // Check In Start Column Definition
+    @ColumnInfo(name = "CheckInStart")
+    private Time CheckInStart;
+
+    // Check In Start Methods
+    public Time getCheckInStart() { return CheckInStart; }
+    public void setCheckInStart(Time checkInStart) { this.CheckInStart = checkInStart; }
+
+    // Check In End Column Definition
+    @ColumnInfo(name = "CheckInEnd")
+    private Time CheckInEnd;
+
+    // Check In End Methods
+    public Time getCheckInEnd() { return CheckInEnd; }
+    public void setCheckInEnd(Time checkInEnd) { this.CheckInStart = checkInEnd; }
+
+    // PatternOne Column Definition
+    @ColumnInfo(name = "PatternOne")
+    private int PatternOne;
+
+    // PatternOne Methods
+    public int getPatternOne() { return PatternOne; }
+    public void setPatternOne(int patternOne) { this.PatternOne = patternOne; }
+
+    // PatternTwo Column Definition
+    @ColumnInfo(name = "PatternTwo")
+    private int PatternTwo;
+
+    // PatternTwo Methods
+    public int getPatternTwo() { return PatternTwo; }
+    public void setPatternTwo(int patternTwo) { this.PatternTwo = patternTwo; }
+
+
     // Override Methods
     @Override
-    public String toString() { return EmergencyText; }
+    public String toString() { return String.valueOf(AppMode); }
 }
